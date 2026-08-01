7d4e4c7d76eae72e97835024171bcafbe24fcb26
Sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-08-01 18:42:35 +0530

    A04: Completed assignment

---
+++ A04/Program.cs
@@ -0,0 +1,30 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------------------------------------
+// Program.cs
+// Reads the word list and gets the top 7 most frequent characters.
+
+#region Program -----------------------------------------------------------------------------------
+/// <summary>
+/// Opens up a file and iterates over characters to increase their frequency table.
+/// Sorts the table by value (frequency) and then takes the Top 7 (key, value) pairs.
+/// </summary>
+
+Dictionary<char, int> freqTable = new ();
+string content = File.ReadAllText (@"C:\Users\msara\Downloads\Words-List.txt");
+
+foreach (var ch in content) {
+   if (ch is >= 'A' and <= 'Z') {
+      if (freqTable.TryGetValue (ch, out int value))
+         freqTable[ch]++;
+      else
+         freqTable[ch] = 1;
+   }
+}
+
+Console.WriteLine ("Printing Top 7 Elements..");
+foreach (var pair in freqTable.OrderByDescending (item => item.Value).Take (7)) {
+   Console.WriteLine ($"character {pair.Key} was used {pair.Value} times");
+}
+#endregion
