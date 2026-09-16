9a154267ce57b30aac2b024d86a2babe9a869753
sadhasivam <Sadhasivam.Saravanan@trumpf.com>
2026-09-16 10:41:46 +0530

    Formatted Version

---
+++ A15/PriorityQueue.cs
@@ -0,0 +1,69 @@
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2026.
+// Copyright (c) TRUMPF Metamation India.
+// ------------------------------------------------------------------------------------------------
+// PriorityQueue.cs
+// Custom PriorityQueue implementation.
+// ------------------------------------------------------------------------------------------------
+namespace CustomPriorityQueue;
+
+#region Class MyPriorityQueue ---------------------------------------------------------------------
+/// <summary>Implements a custom min-heap priority queue.</summary>
+class MyPriorityQueue<T> where T : IComparable<T> {
+   #region Properties -----------------------------------------------
+   /// <summary>Returns true if the heap is empty; otherwise, false.</summary>
+   public bool IsEmpty => mHeap.Count == 0;
+   #endregion
+
+   #region Methods --------------------------------------------------
+   /// <summary>Adds an element to the queue.</summary>
+   /// <param name="value">The item to add.</param>
+   public void Enqueue (T value) {
+      mHeap.Add (value);
+      SiftUp ();
+   }
+
+   /// <summary>Removes and returns the smallest element.</summary>
+   /// <returns>The minimum element in the queue.</returns>
+   /// <exception cref="InvalidOperationException">Thrown when the queue is empty.</exception>
+   public T Dequeue () {
+      if (IsEmpty) throw new InvalidOperationException ("Queue is empty.");
+      var minValue = mHeap[0];
+      SiftDown ();
+      return minValue;
+   }
+   #endregion
+
+   #region Implementation -------------------------------------------
+   // Restores heap property by moving the root element down.
+   private void SiftDown () {
+      mHeap[0] = mHeap[^1];
+      mHeap.RemoveAt (mHeap.Count - 1);
+      var (currentIndex, leftChild) = (0, 1);
+      while (leftChild < mHeap.Count) {
+         var (minIndex, rightChild) = (leftChild, 2 * currentIndex + 2);
+         if (rightChild < mHeap.Count && mHeap[rightChild].CompareTo (mHeap[leftChild]) < 0)
+            minIndex = rightChild;
+         if (mHeap[currentIndex].CompareTo (mHeap[minIndex]) <= 0) break;
+         (mHeap[currentIndex], mHeap[minIndex]) = (mHeap[minIndex], mHeap[currentIndex]);
+         (currentIndex , leftChild) = (minIndex, 2 * minIndex + 1);
+      }
+   }
+
+   // Restores heap property by moving the last element up.
+   private void SiftUp () {
+      int childIndex = mHeap.Count - 1;
+      while (childIndex > 0) {
+         int parentIndex = (childIndex - 1) / 2;
+         if (mHeap[childIndex].CompareTo (mHeap[parentIndex]) >= 0) break;
+         (mHeap[childIndex], mHeap[parentIndex]) = (mHeap[parentIndex], mHeap[childIndex]);
+         childIndex = parentIndex;
+      }
+   }
+   #endregion
+
+   #region Fields ---------------------------------------------------
+   List<T> mHeap = [];
+   #endregion
+}
+#endregion
--- A15/Program.cs
+++ A15/Program.cs
@@ -1,61 +1,8 @@
-﻿var hq = new Heap<int> ();
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-hq.Enqueue (99);
-hq.Enqueue (9);
-hq.Enqueue (24);
-hq.Enqueue (18);
-
-hq.PrintHeapQ ();
-
-Console.WriteLine (hq.Dequeue ());
-hq.PrintHeapQ ();
-
-class Heap<T> where T : IComparable<T> {
-   List<T> mHeap = new();
-   public void Enqueue (T value) {
-      mHeap.Add (value);
-      SiftUp ();
-   }
-
-   public void PrintHeapQ() {
-      Console.WriteLine ("Printing..");
-      foreach(var item in mHeap) {
-         Console.Write($"{item} ");
-      }
-      Console.WriteLine ("- - - - - - -");
-   }
-   public T Dequeue () {
-      if (IsEmpty) throw new Exception ("Empty HeapQ");
-      var value = mHeap[0];
-      SiftDown ();
-      return value;
-   }
-
-   public bool IsEmpty => mHeap.Count == 0;
-
-   public void SiftDown () {
-      (mHeap[mHeap.Count - 1], mHeap[0]) = (mHeap[0], mHeap[mHeap.Count - 1]);
-      mHeap.RemoveAt (mHeap.Count-1);
-      int currentPos = 0;
-      int leftChild = 2*currentPos+1;
-      while(leftChild<=mHeap.Count-1) {
-         int minimum = leftChild;
-         int rightChild = 2 * currentPos + 2;
-         if(rightChild<=mHeap.Count-1 && mHeap[rightChild].CompareTo (mHeap[leftChild])<0) {
-            minimum = rightChild;
-         }
-         if (mHeap[currentPos].CompareTo (mHeap[minimum]) < 1) break;
-         (mHeap[currentPos], mHeap[minimum]) = (mHeap[minimum], mHeap[currentPos]);
-         currentPos = minimum;
-         leftChild = 2 * currentPos + 1;
-      }
-   }
-
-   public void SiftUp () {
-      int Cp = mHeap.Count-1;
-      while (Cp > 0 && mHeap[Cp].CompareTo (mHeap[(Cp-1) / 2]) < 0) {
-         (mHeap[Cp], mHeap[(Cp - 1)/2]) = (mHeap[(Cp - 1)/ 2], mHeap[Cp]);
-         Cp = (Cp - 1) / 2;
-      }
+namespace A15 {
+   internal class Program {
    }
 }
